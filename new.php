26600d4d494196b591be8d087d0759f1646df942
Daniel <daniel@natur-kultur.eu>
2014-07-30 21:17:06 +0200

    :(

--- new.php
+++ new.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width" />
+  <!--<meta name="viewport" content="width=device-width" />-->
   <script type="text/javascript"> // check for local copies of jquery, otherwise use the CDN hosted version. Make the form draggable and resizeable.
   /* Minimized version of https://raw.githubusercontent.com/kvz/phpjs/master/experimental/filesystem/file_exists.js */function file_exists(a){var b=this.window.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();if(!b){throw new Error("XMLHttpRequest not supported")}b.open("HEAD",a,false);b.send(null);if(b.status==200){return true}return false};
   var local_sources = Array("/jquery.min.js","/jquery-ui-1.10.3/themes/base/jquery-ui.css","/jquery-ui-1.10.3/ui/minified/jquery-ui.min.js","jqtp.js"),
